57fdee0a6115c7f4d16db068bbe203a27020ccd2
shao <shaochewingice@gmail.com>
2022-06-14 19:54:38 +0000

    network

--- network.tf
+++ network.tf
@@ -1,10 +1,11 @@
-data "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example" {
   name     = "shaoliu-workshop"
+  location = "Canada Central"
 }
 
 module "network" {
   source  = "app.terraform.io/hashicat-azureshao/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = data.azurerm_resource_group.example.id
+  resource_group_name = azurerm_resource_group.example.name
 }
